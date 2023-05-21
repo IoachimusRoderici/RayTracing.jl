5526fb839fc6ac83151ad26be63d2f8823818783
Joaquín Rodríguez <joaq.rod0@gmail.com>
2023-05-21 17:50:23 -0300

    definimos las variables de la simulación

--- RT_simulacion.c
+++ RT_simulacion.c
@@ -2,8 +2,18 @@
 
 /* Variables de la Simulación */
 
-static gsl_vector_long_double *pos; //posición actual del rayo
-static gsl_vector_long_double *dir; //dirección actual del rayo (vector unitario)
+static bool registrar_recorrido;            //si es true guardamos una lista de los rebotes
+static unsigned int max_rebotes;            //abortar la simulación si llega a este número de rebotes
 
-static struct RT_parametros config; //parámetros pasados por el usuario
-static struct RT_resultados data;   //información que devolver al usuario
+static unsigned int dims;                   //número de dimensiones
+static gsl_vector_long_double *dir_inicial; //vector unitario, o nulo
+static gsl_matrix *cuerpos;                 //lista de coordenadas de los centros de los átomos
+
+static double radio_cuerpo;                 //radio de los átomos
+static double radio_estrella;               //radio de la estrella
+
+static gsl_vector_long_double *pos;         //posición actual del rayo
+static gsl_vector_long_double *dir;         //dirección actual del rayo (vector unitario)
+
+unsigned int rebotes;                       //cuantas veces rebotamos hasta ahora
+long double distancia;                      //distancia recorrida hasta ahora

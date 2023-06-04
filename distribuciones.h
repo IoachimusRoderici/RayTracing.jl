c2edacb1f2e3aa856d74c7d682229fa4cc87be5f
Joaquín Rodríguez <joaq.rod0@gmail.com>
2023-06-03 21:22:43 -0300

    added distribuciones

---
+++ distribuciones.h
@@ -0,0 +1,5 @@
+
+/* Estas funciones generan distintas distribuciones de puntos.
+   Para cada distribución hay una función que devuelve un array con los
+   puntos, y otra que los escribe en un archivo de texto.
+*/

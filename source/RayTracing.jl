93daed15682980d70860d8e3d66e9ff8b453f95b
Joaquín Rodríguez <joaq.rod0@gmail.com>
2024-12-06 16:54:40 -0300

    Actualizar interfaz de superficies, usar HyperSphere por el momento.

--- source/RayTracing.jl
+++ source/RayTracing.jl
@@ -1,14 +1,16 @@
 module RayTracing
 
 using StaticArrays
-import LinearAlgebra: normalize
-import GeometryBasics: Point
+import LinearAlgebra: normalize, ⋅
+import GeometryBasics: Point, HyperSphere, origin, radius
 
 
 include("rays/AbstractRay.jl")
 include("rays/BasicRay.jl")
 
 include("surfaces/AbstractSurface.jl")
-include("surfaces/Sphere.jl")
+
+include("intersections/intersections.jl")
+include("intersections/Sphere_intersections.jl")
 
 end
--- source/surfaces/AbstractSurface.jl
+++ source/surfaces/AbstractSurface.jl
@@ -1,14 +1,18 @@
 export AbstractSurface
 
 """
-Represents an ND-dimensional surface that rays can bounce on.
+Represents an `ND`-dimensional surface that rays can bounce on.
 """
 abstract type AbstractSurface{ND} end
 
 """
     normal(surface, point)
 
-Return the outward normal vector to the surface at the given point,
-assuming the point in contained in the surface.
+Return a vector normal to `surface` at `point`, assuming `point`
+is contained in the surface.
+
+For closed surfaces, return the outward-pointing normal vector.
 """
 function normal end
+
+normal(sphere::HyperSphere, point) = normalize(point - origin(sphere))

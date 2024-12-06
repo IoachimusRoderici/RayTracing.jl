d0f81d2659968c2fb216a8da47d21ff9373a6c5d
Joaquín Rodríguez <joaq.rod0@gmail.com>
2024-12-06 17:05:15 -0300

    Crear interfaz de intersecciones, implementar interseccion con esfera

---
+++ source/intersections/intersections.jl
@@ -0,0 +1,25 @@
+"""
+    intersection_distance(surface, ray)
+
+Return the distance from `position(ray)` to the first point of intersection
+between `surface` and `ray`, or infinity if there is no intersection.
+"""
+function intersection_distance end
+
+"""
+    intersection(surface, ray)
+
+Return the first point of intersection between `surface` and `ray`,
+or `nothing` if there is no intersection.
+"""
+function intersection end
+
+function intersection(surface, ray)
+    d = intersection_distance(surface, ray)
+
+    if d < Inf
+        return position(ray) + d * direction(ray)
+    else
+        return nothing
+    end
+end

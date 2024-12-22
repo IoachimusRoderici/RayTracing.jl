a07d4cb8ff21124daaa080998399b7d4a500928d
Joaquín Rodríguez <joaq.rod0@gmail.com>
2024-12-22 15:09:46 -0300

    add eltype and ndims methods for AbstractRay

--- src/rays/AbstractRay.jl
+++ src/rays/AbstractRay.jl
@@ -10,8 +10,11 @@ A ray has a position and a direction, and may carry additional information.
 """
 abstract type AbstractRay{ND, T <: AbstractFloat} end
 
+Base.eltype(::Type{<:AbstractRay{ND, T}}) where {ND, T} = T
+Base.ndims(::Type{<:AbstractRay{ND, T}}) where {ND, T} = ND
+
 """
-    position(ray)
+position(ray)
 
 Return the current position of the ray.
 """
@@ -19,14 +22,14 @@ function position end
 Base.position(ray::AbstractRay) = position(ray)
 
 """
-    direction(ray)
+direction(ray)
 
 Return the current direction of the ray.
 """
 function direction end
 
 """
-    advance!(ray, distance)
+advance!(ray, distance)
 
 Advances the ray by the given distance int its current direction.
 """

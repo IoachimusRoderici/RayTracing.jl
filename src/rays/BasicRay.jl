0c05307cff5ee35abb9988980d956a9031b8f7f0
Joaquín Rodríguez <joaq.rod0@gmail.com>
2024-12-22 15:10:25 -0300

    BasicRay interface corrections

--- src/rays/BasicRay.jl
+++ src/rays/BasicRay.jl
@@ -11,15 +11,15 @@ struct BasicRay{ND, T} <: AbstractRay{ND, T}
 
     function BasicRay{ND, T}(pos, dir) where {ND, T}
         normalized_dir = normalize(dir)
-        @assert norm(normalized_dir) ≉ 1 "Could not normalize dir: got $normalized_dir."
+        @assert norm(normalized_dir) ≈ 1 "Could not normalize dir: got $normalized_dir."
         new{ND, T}(copy(pos), normalized_dir)
     end
 end
 
 BasicRay(pos, dir) = BasicRay{length(pos), promote_type(eltype(pos), eltype(dir))}(pos, dir)
 
-position(ray::BasicRay) = Point(ray.pos)
-direction(ray::BasicRay) = SVector(ray.dir)
+position(ray::BasicRay) = ray.pos
+direction(ray::BasicRay) = ray.dir
 
 function advance!(ray::BasicRay, distance)
     ray.pos .+= distance * direction(ray)

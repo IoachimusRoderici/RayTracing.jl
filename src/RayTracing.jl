0b36c367d555c7314f1428008a4ee7705635f36e
Joaquín Rodríguez <joaq.rod0@gmail.com>
2024-12-27 14:07:07 -0300

    Add ray type StepRecorder

--- src/RayTracing.jl
+++ src/RayTracing.jl
@@ -1,12 +1,13 @@
 module RayTracing
 
 using StaticArrays
-import LinearAlgebra: norm, normalize, ⋅
+import LinearAlgebra: norm, normalize, normalize!, ⋅
 import GeometryBasics: Point, HyperSphere, origin, radius
 
 
 include("rays/AbstractRay.jl")
 include("rays/BasicRay.jl")
+include("rays/StepRecorder.jl")
 
 include("surfaces/AbstractSurface.jl")
 
---
+++ src/rays/StepRecorder.jl
@@ -0,0 +1,25 @@
+export StepRecorder
+
+"""
+Wrapper around a ray type, `R`, that records the trajectory of the ray.
+
+A new point is added to the trajectory after every call to `advance!`.
+"""
+struct StepRecorder{ND, T, R <: AbstractRay{ND, T}} <: AbstractRay{ND, T}
+    ray :: R
+    steps :: Vector{Point{ND, T}}
+
+    StepRecorder{ND, T, R}(ray :: R) where {ND, T, R} = new{ND, T, R}(ray, Point{ND, T}[position(ray)])
+end
+
+StepRecorder(ray) = StepRecorder{ndims(ray), eltype(ray), typeof(ray)}(ray)
+
+position(ray::StepRecorder) = position(ray.ray)
+direction(ray::StepRecorder) = direction(ray.ray)
+reflect!(ray::StepRecorder, normal) = reflect!(ray.ray, normal)
+
+function advance!(ray::StepRecorder, distance)
+    advance!(ray.ray, distance)
+    push!(ray.steps, position(ray))
+    println("advanced $distance")
+end

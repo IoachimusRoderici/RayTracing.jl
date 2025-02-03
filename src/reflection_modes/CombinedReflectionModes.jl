941fb9dbf0ff7d9ef64c1341c761c8d9f2915d37
Joaquín Rodríguez <joaq.rod0@gmail.com>
2025-02-03 13:16:41 -0300

    Create CombinedReflectionModes

---
+++ src/reflection_modes/CombinedReflectionModes.jl
@@ -0,0 +1,39 @@
+export CombinedReflectionModes
+
+"""
+    CombinedReflectionModes{Modes <: Tuple{Vararg{ReflectionMode}}} <: ReflectionMode
+
+A [`ReflectionMode`](@ref) that combines the effects of different reflection
+modes.
+
+Calling [`reflect!`](@ref) with this mode results in a call to `reflect!` with
+each contained mode.
+
+## Example
+
+```jldoctest
+struct Mode1 <: ReflectionMode end
+struct Mode2 <: ReflectionMode end
+RayTracing.reflect!(ray, mode::Mode1, geometric_data, material_data) = println(material_data)
+RayTracing.reflect!(ray, mode::Mode2, geometric_data, material_data) = println(geometric_data)
+
+combined_mode = CombinedReflectionModes(Mode1(), Mode2())
+reflect!(:some_ray, combined_mode, "some geometric data", "some material data")
+
+# output
+
+some material data
+some geometric data
+```
+"""
+struct CombinedReflectionModes{Modes <: Tuple{Vararg{ReflectionMode}}} <: ReflectionMode
+    modes :: Modes
+end
+
+CombinedReflectionModes(modes...) = CombinedReflectionModes(modes)
+
+function reflect!(ray, mode::CombinedReflectionModes, geometric_data, material_data)
+    for m in mode.modes
+        reflect!(ray, m, geometric_data, material_data)
+    end
+end
